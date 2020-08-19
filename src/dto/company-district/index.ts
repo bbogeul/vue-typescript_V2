7377f13b868cc50b72ba532db6d632ebe01058c3
RIA <kbr@wiedaehan.com>
2020-08-19 10:27:06 +0900

    fixed file name

--- src/dto/company-district/index.ts
+++ src/dto/company-district/index.ts
@@ -1,3 +1,3 @@
 export * from './company-district.dto';
 export * from './company-district-list.dto';
-export * from './company-disctrict-update.dto';
+export * from './company-district-update.dto';
