33479cb994f862d6cd3a4ce942ce9a8f94ce1ab0
nanuda <nanuda@widaehan.com>
2020-08-31 11:44:21 +0900

    test CI/CD

--- src/modules/_layouts/Layout/LoginLayout.layout.vue
+++ src/modules/_layouts/Layout/LoginLayout.layout.vue
@@ -1,12 +1,7 @@
 <template>
   <section class="body text-center">
     <div class="form-signin" v-on:keyup.enter="signin()">
-      <img
-        class="mb-4 logo"
-        src="https://nanudakitchen.com/_nuxt/img/65bad5b.svg"
-        alt
-        width="72"
-      />
+      <img class="mb-4 logo" src="https://nanudakitchen.com/_nuxt/img/65bad5b.svg" alt width="72" />
       <div>
         <label for="inputPhone" class="sr-only">휴대폰 번호</label>
         <input
@@ -36,18 +31,19 @@
           name="checkbox-1"
           :value="true"
           :unchecked-value="false"
-          >Stay signed in</b-form-checkbox
-        >
+        >Stay signed in</b-form-checkbox>
       </div>
       <button
         @click="signin()"
         v-on:keyup.enter="signin()"
         class="btn btn-lg btn-primary btn-block mt-1"
-      >
-        로그인
-      </button>
-      <span style="font-size: 10px; margin-bottom:5px">
+      >로그인</button>
+      <span style="font-size: 11px; margin-bottom:5px">
         <i>기존 나누다키친 관리자 계정으로 로그인 하시면 됩니다</i>
+      </span>
+      <br />
+      <span style="font-size: 11px; margin-bottom:5px">
+        <i>로그인하시는 부분에 있어 문제가 발생하면 시스템팀에게 문의 바랍니다</i>
       </span>
       <p class="mt-5 mb-3 text-muted">
         &copy;
